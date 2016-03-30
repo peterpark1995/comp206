b2b0c4f3f8bda285a5c6fd33ef2fecc947de3479
Peter Park <peterpark@Peters-MacBook-Pro.local>
2016-03-29 23:15:16 -0400

    A3Q2 added

--- A3Q2.c
+++ A3Q2.c
@@ -1,64 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<time.h>
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-int main()
+int main(int argc, char *argv[])
 {
- printf("start..\n");
- //getting stored environment
- char *BackupSource = getenv("BackupSource");
- char *BackupDestination = getenv("BackupDestination");
- char *BackupTime = getenv("BackupTime");
- char buffer[100];
+  int b=1;
+  char backup[100];
+  char *source=getenv("BackupSource");
+  char *destination=getenv("BackupDestination");
+  char *btime=getenv("BackupTime");
 
+  time_t getTime;
+  struct tm *actualTime;
 
- //declaring tm pointer
-struct tm *time_structure;
-//time_t epoch seems to be standard syntax
-time_t epoch;
+   while(b)
+    {
+       //in each loop you get the time so it can be compared with the env variable
+       time(&getTime);
+       actualTime=localtime(&getTime);
+       strftime(backup, 100, "%H:%M", actualTime);
 
-
-while(1)
-{   
-    //storing the localtime on the deference of *getTime
-    time_structure = localtime(&epoch);
-    //values of time in seconds
-    time(&epoch);
-    //storing the string of the time on hour and minute
-    strftime(buffer, 100, "%H:%M", time_structure);
-    while(strcmp(buffer,BackupTime)==0)
-    {  
-        system("cp -r $BackupSource $BackupDestination");
+       //no need for a while loop in a while loop
+       if(strcmp(backup,btime)==0)
+       {
+           system("cp -r $BackupSource $BackupDestination");
+       }
+       sleep(60);
     }
-    sleep(60);
+
+return 0;
 }
-
- printf("Done.\n");
- return(0);
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
